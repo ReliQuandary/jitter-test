0f58d3fec4b79daf1c65bf3b04f6d2740efb4bbe
Joshitha L M <joshithalm@gmail.com>
2024-02-21 12:35:52 -0500

    Added TMP + a reload scene button

--- Jitter Test/Assets/Scripts/SceneController.cs
+++ Jitter Test/Assets/Scripts/SceneController.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         if (Application.platform == RuntimePlatform.Android) {
             Application.targetFrameRate = 60;
         } else {
             Application.targetFrameRate = -1;
         }
+    }
+
+    public void ReloadScene() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
